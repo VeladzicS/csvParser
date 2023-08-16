2f1f299d00e9548be64ded1cb4e4bc506446c6fc
Senahid Velazic <veladzicsenahid@gmail.com>
2023-08-16 17:02:01 +0200

    .

--- app.js
+++ app.js
@@ -21,13 +21,24 @@ async function generatePdfForGroup(headers, groupRows, groupName) {
 
     fs.writeFileSync(`${outputFolderPath}/${groupName}.pdf`, pdfBuffer);
 }
+
+async function generatePdfForBreed(headers, breedRows, breedName) {
+    const html = await ejs.renderFile('./views/table.ejs', { headers, rows: breedRows, showButton: false });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0', timeout: 160000 });
+
+    const pdfBuffer = await page.pdf({ format: 'A4', timeout: 160000 });
+    await browser.close();
+
+    fs.writeFileSync(`${outputFolderPath}/${breedName}.pdf`, pdfBuffer);
+}
 if (!fs.existsSync(outputFolderPath)) {
     fs.mkdirSync(outputFolderPath);
 }
 
 app.get('/generate-pdf', async (req, res) => {
-    const csvFile = './Feb23.csv';
-
+    const csvFile = './June23.csv';
     const headers = [];
     const rows = [];
     const groups = {};
@@ -38,13 +49,16 @@ app.get('/generate-pdf', async (req, res) => {
             headers.push(...csvHeaders);
         })
         .on('data', (row) => {
-            rows.push(row);
-            if (row.Group) {
-                const group = row.Group;
-                if (groups[group]) {
-                    groups[group].push(row);
-                } else {
-                    groups[group] = [row];
+            const allEmptyCells = Object.values(row).every(cell => cell.trim() === '');
+            if (!allEmptyCells) {
+                rows.push(row);
+                if (row.Group) {
+                    const group = row.Group;
+                    if (groups[group]) {
+                        groups[group].push(row);
+                    } else {
+                        groups[group] = [row];
+                    }
                 }
             }
         })
@@ -57,9 +71,52 @@ app.get('/generate-pdf', async (req, res) => {
 });
 
 
-app.get('/table', async (req, res) => {
-    const csvFile = './Feb23.csv';
+app.get('/generate-breed-pdf', async (req, res) => {
+    const csvFile = './June23.csv';
+    const breedFilter = req.query.breed;
+    const headers = [];
+    const rows = [];
+    const breeds = {};
 
+    fs.createReadStream(csvFile)
+        .pipe(csvParser())
+        .on('headers', (csvHeaders) => {
+            headers.push(...csvHeaders);
+        })
+        .on('data', (row) => {
+            const allEmptyCells = Object.values(row).every(cell => cell.trim() === '');
+            if (!allEmptyCells && row.Breed) {
+                if (!breedFilter || row.Breed === breedFilter) {
+                    rows.push(row);
+                    const breed = row.Breed;
+                    if (breeds[breed]) {
+                        breeds[breed].push(row);
+                    } else {
+                        breeds[breed] = [row];
+                    }
+                }
+            }
+        })
+        .on('end', async () => {
+            if (breedFilter && breeds[breedFilter]) {
+                await generatePdfForBreed(headers, breeds[breedFilter], breedFilter);
+                res.send(`PDF for ${breedFilter} breed generated.`);
+            } else if (breedFilter && !breeds[breedFilter]) {
+                res.status(400).send('No data available for this breed.');
+            } else {
+                for (const breedName in breeds) {
+                    await generatePdfForBreed(headers, breeds[breedName], breedName);
+                }
+                res.send('PDFs for all breeds generated.');
+            }
+        });
+});
+
+
+
+app.get('/table', async (req, res) => {
+    const csvFile = './June23.csv';
+    const breedFilter = req.query.breed;
     const headers = [];
     const rows = [];
     const uniqueAddresses = new Set();
@@ -71,9 +128,12 @@ app.get('/table', async (req, res) => {
             headers.push(...csvHeaders);
         })
         .on('data', (row) => {
-            rows.push(row);
-            uniqueAddresses.add(row.Address);
-            uniqueAddresses2.add(row['Address2']);
+            const allEmptyCells = Object.values(row).every(cell => cell.trim() === '');
+            if (!allEmptyCells && (!breedFilter || row.Breed === breedFilter)) {
+                rows.push(row);
+                uniqueAddresses.add(row.Address);
+                uniqueAddresses2.add(row['Address2']);
+            }
         })
         .on('end', async () => {
             const overlappingAddresses = new Set([...uniqueAddresses].filter(x => uniqueAddresses2.has(x)));
@@ -89,6 +149,110 @@ app.get('/table', async (req, res) => {
             });
         });
 });
+
+app.get('/first-three-breeds', async (req, res) => {
+    const csvFile = './June23.csv';
+
+    const headers = [];
+    const rows = [];
+    const uniqueBreeds = new Set();
+
+    fs.createReadStream(csvFile)
+        .pipe(csvParser())
+        .on('headers', (csvHeaders) => {
+            headers.push(...csvHeaders);
+        })
+        .on('data', (row) => {
+            // Check if the row has all empty cells
+            const allEmptyCells = Object.values(row).every((cell) => cell.trim() === '');
+            if (!allEmptyCells) {
+                rows.push(row);
+                uniqueBreeds.add(row.Breed);
+            }
+        })
+        .on('end', async () => {
+            const limitedRows = [];
+            const breedCount = {};
+
+            for (const row of rows) {
+                const breed = row.Breed;
+                breedCount[breed] = breedCount[breed] ? breedCount[breed] + 1 : 1;
+
+                if (breedCount[breed] <= 3) {
+                    limitedRows.push(row);
+                }
+            }
+
+            res.render('table', {
+                headers,
+                rows: limitedRows,
+                showButton: false,
+            });
+        });
+});
+
+app.get('/generate-pdf-first-three', async (req, res) => {
+    const csvFile = './June23.csv';
+
+    const headers = [];
+    const rows = [];
+    const groups = {};
+    const uniqueBreeds = new Set();
+
+    fs.createReadStream(csvFile)
+        .pipe(csvParser())
+        .on('headers', (csvHeaders) => {
+            headers.push(...csvHeaders);
+        })
+        .on('data', (row) => {
+            const allEmptyCells = Object.values(row).every((cell) => cell.trim() === '');
+            if (!allEmptyCells) {
+                rows.push(row);
+                if (row.Group) {
+                    const group = row.Group;
+                    if (groups[group]) {
+                        groups[group].push(row);
+                    } else {
+                        groups[group] = [row];
+                    }
+                }
+                uniqueBreeds.add(row.Breed);
+            }
+        })
+        .on('end', async () => {
+            const limitedGroups = {};
+
+            for (const groupName in groups) {
+                const groupRows = groups[groupName];
+                const limitedRows = [];
+                const breedCount = {};
+
+                for (const row of groupRows) {
+                    const breed = row.Breed;
+                    breedCount[breed] = breedCount[breed] ? breedCount[breed] + 1 : 1;
+
+                    if (breedCount[breed] <= 3) {
+                        limitedRows.push(row);
+                    }
+                }
+
+                limitedGroups[groupName] = limitedRows;
+            }
+
+            const groupPromises = [];
+
+            for (const groupName in limitedGroups) {
+                const groupRows = limitedGroups[groupName];
+                const promise = generatePdfForGroup(headers, groupRows, groupName);
+                groupPromises.push(promise);
+            }
+
+            await Promise.all(groupPromises);
+            res.send('PDFs generated.');
+        });
+});
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
